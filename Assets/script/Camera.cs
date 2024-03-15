0bc016c2086a4e9003f2da8f1406b1801948476a
moruton1119 <moruton3@gmail.com>
2024-03-15 12:19:31 +0900

    ARカメラを使った方向にシフト

--- Assets/script/Camera.cs
+++ Assets/script/Camera.cs
@@ -4,20 +4,21 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Threading.Tasks;
 using System.IO;
-using UnityEngine.EventSystems;
 
 public class Camera : MonoBehaviour
 {
-    // カメラの映像を映す
     public RawImage rawImage;
     WebCamTexture webCamTexture;
-    public GameObject Scene;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        // カメラ画面の取得
-        webCamTexture = new WebCamTexture();
+        if (webCamTexture == null)
+        {
+            // カメラ画面の取得
+            webCamTexture = new WebCamTexture();
+        }
         // カメラ画面を画像に描画
         rawImage.texture = webCamTexture;
         // カメラを起動
@@ -26,55 +27,7 @@ public class Camera : MonoBehaviour
     public async void Shutter()
     {
         await Task.Delay(3000);
-        webCamTexture.Pause();
         this.GetComponent<SceneChange>().OnLoadPicture();
-        save();
+        this.GetComponent<CameraManager>().save();
     }
-
-    public void save()
-    {
-        // Texture2Dを新規作成
-        Texture2D outputTexture = new Texture2D(webCamTexture.width, webCamTexture.height, TextureFormat.ARGB32, false);
-        
-
-        // カメラのピクセルデータを設定
-        // Color[] colors = webCamTexture.GetPixels();
-        // texture.SetPixels(colors);
-        // TextureをApply
-        // texture.Apply();
-
-        Color[] inputColors = webCamTexture.GetPixels();
-        Color[] outputColors = new Color[webCamTexture.width * webCamTexture.height];
-        for (int y = 0; y < webCamTexture.height; y++)
-        {
-            for (int x = 0; x < webCamTexture.width; x++)
-            {
-                var color = inputColors[(webCamTexture.width * y) + x];
-                float average = (color.r + color.g + color.b) / 3;
-                outputColors[(webCamTexture.width * y) + x] = new Color(average, average, average);
-            }
-        }
-        outputTexture.SetPixels(outputColors);
-        outputTexture.Apply();
-
-        rawImage.texture = outputTexture;
-
-
-
-
-        // Encode
-        byte[] bin = outputTexture.EncodeToJPG();
-        // Encodeが終わったら削除
-        // Object.Destroy(outputTexture);
-
-        // ファイルを保存
-    #if UNITY_ANDROID
-        File.WriteAllBytes(Application.persistentDataPath + "/test.jpg", bin);
-    #endif
-        File.WriteAllBytes(Application.dataPath + "/test.jpg", bin);
-        Debug.Log("画像書き込んだよ");
-
-    }
-
-
 }
