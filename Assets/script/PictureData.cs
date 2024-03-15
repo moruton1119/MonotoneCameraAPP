98ca2dd2c09c06b560c0002583d9a4f41a446f2a
moruton1119 <moruton3@gmail.com>
2024-03-16 02:43:59 +0900

    写真の角度がおかしかったのを修正

--- Assets/script/PictureData.cs
+++ Assets/script/PictureData.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class PictureData : MonoBehaviour
 {
@@ -26,9 +27,47 @@ public class PictureData : MonoBehaviour
         CameraImageExample CIE  = FindObjectOfType<CameraImageExample>();
     }
     void Update()
-        {
-        Texture2D outputTexture = CameraImageExample.outputTexture;
-            
+        { 
         }
+
+    public void save()
+    {
+
+        Texture2D C_data = CameraImageExample.saveTex;
+        outputTexture = new Texture2D(C_data.width, C_data.height, TextureFormat.ARGB32, false);
+        // カメラのピクセルデータを設定
+        Color[] inputColors = C_data.GetPixels();
+        Color[] outputColors = new Color[C_data.width * C_data.height];
+        // //モノクロ処理
+        for (int y = 0; y < C_data.height; y++)
+        {
+            for (int x = 0; x < C_data.width; x++)
+            {
+                var color = inputColors[(C_data.width * y) + x];
+                float average = (color.r + color.g + color.b) / 3;
+                outputColors[(C_data.width * y) + x] = new Color(average, average, average);
+            }
+        }
+        outputTexture.SetPixels(outputColors);
+        outputTexture.Apply();
+
+        // Encode
+        byte[] bin = outputTexture.EncodeToJPG();
+        // Encodeが終わったら削除
+        // Object.Destroy(outputTexture);
+
+        // ファイルを保存
+    #if UNITY_ANDROID
+        
+        NativeGallery.SaveImageToGallery(outputTexture, "MonotoneCamera", "test.jpg");
+
+
+        Debug.Log("android画像書き込んだよ");
+
+    #endif
+        File.WriteAllBytes(Application.dataPath + "/test.jpg", bin);
+        Debug.Log("画像書き込んだよ");
+
+    }  
 
 }
